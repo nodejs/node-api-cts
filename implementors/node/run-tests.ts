468d868a2b5c6e3e97999690b72804c0b86b8928
Kræn Hansen <mail@kraenhansen.dk>
2025-12-22 02:56:17 +0100

    Add function arguments test (#18)
    * Build, load and test function arguments addon
    
    * Add building addons to CI workflow
    
    * Use cmake directly
    
    * Fix Windows specific issues
    
    * Configure using the default generator
    
    * Run tests on MacOS and Windows too
    
    * Ignore Windows debug symbols
    
    * Fix Windows issue importing ts-strip.js
    
    * Delete win_delay_load_hook.cc for now
    
    * Removed the MSVC conditional DELAYLOAD
    
    As per https://github.com/nodejs/node-gyp/blob/db5385c5467e5bfb914b9954f0313c46f1f4e10d/addon.gypi#L68-L89

--- implementors/node/run-tests.ts
+++ implementors/node/run-tests.ts
@@ -5,11 +5,18 @@ import { test, type TestContext } from "node:test";
 
 const ROOT_PATH = path.resolve(import.meta.dirname, "..", "..");
 const TESTS_ROOT_PATH = path.join(ROOT_PATH, "tests");
+
 const ASSERT_MODULE_PATH = path.join(
   ROOT_PATH,
   "implementors",
   "node",
   "assert.js"
+);
+const LOAD_ADDON_MODULE_PATH = path.join(
+  ROOT_PATH,
+  "implementors",
+  "node",
+  "load-addon.js"
 );
 
 async function listDirectoryEntries(dir: string) {
@@ -31,13 +38,20 @@ async function listDirectoryEntries(dir: string) {
   return { directories, files };
 }
 
-function runFileInSubprocess(filePath: string): Promise<void> {
+function runFileInSubprocess(cwd: string, filePath: string): Promise<void> {
   return new Promise((resolve, reject) => {
-    const child = spawn(process.execPath, [
-      "--import",
-      ASSERT_MODULE_PATH,
-      filePath,
-    ]);
+    const child = spawn(
+      process.execPath,
+      [
+        // Using file scheme prefix when to enable imports on Windows
+        "--import",
+        "file://" + ASSERT_MODULE_PATH,
+        "--import",
+        "file://" + LOAD_ADDON_MODULE_PATH,
+        filePath,
+      ],
+      { cwd }
+    );
 
     let stderrOutput = "";
     child.stderr.setEncoding("utf8");
@@ -80,8 +94,7 @@ async function populateSuite(
   const { directories, files } = await listDirectoryEntries(dir);
 
   for (const file of files) {
-    const filePath = path.join(dir, file);
-    await testContext.test(file, () => runFileInSubprocess(filePath));
+    await testContext.test(file, () => runFileInSubprocess(dir, file));
   }
 
   for (const directory of directories) {
