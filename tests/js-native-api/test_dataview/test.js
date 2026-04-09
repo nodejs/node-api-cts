1770fbb7ebabe98a7a593d6e5b1f029844fadcaa
Balakrishna Avulapati <ba@bavulapati.com>
2026-04-09 21:26:02 +0530

    remove the type check in CreateDataView
    Signed-off-by: Balakrishna Avulapati <ba@bavulapati.com>

--- tests/js-native-api/test_dataview/test.js
+++ tests/js-native-api/test_dataview/test.js
@@ -1,48 +1,46 @@
 "use strict";
 
-if (experimentalFeatures.sharedArrayBuffer) {
-  // Testing api calls for arrays
-  const test_dataview = loadAddon("test_dataview");
+// Testing api calls for dataview
+const test_dataview = loadAddon("test_dataview");
 
-  // Test for creating dataview with ArrayBuffer
-  {
-    const buffer = new ArrayBuffer(128);
-    const template = Reflect.construct(DataView, [buffer]);
+// Test for creating dataview with ArrayBuffer
+{
+  const buffer = new ArrayBuffer(128);
+  const template = Reflect.construct(DataView, [buffer]);
 
-    const theDataview = test_dataview.CreateDataViewFromJSDataView(template);
-    assert.ok(
-      theDataview instanceof DataView,
-      `Expect ${theDataview} to be a DataView`,
-    );
-  }
+  const theDataview = test_dataview.CreateDataViewFromJSDataView(template);
+  assert.ok(
+    theDataview instanceof DataView,
+    `Expect ${theDataview} to be a DataView`,
+  );
+}
 
-  // Test for creating dataview with SharedArrayBuffer
-  {
-    const buffer = new SharedArrayBuffer(128);
-    const template = new DataView(buffer);
+// Test for creating dataview with SharedArrayBuffer
+{
+  const buffer = new SharedArrayBuffer(128);
+  const template = new DataView(buffer);
 
-    const theDataview = test_dataview.CreateDataViewFromJSDataView(template);
-    assert.ok(
-      theDataview instanceof DataView,
-      `Expect ${theDataview} to be a DataView`,
-    );
+  const theDataview = test_dataview.CreateDataViewFromJSDataView(template);
+  assert.ok(
+    theDataview instanceof DataView,
+    `Expect ${theDataview} to be a DataView`,
+  );
 
-    assert.strictEqual(template.buffer, theDataview.buffer);
-  }
+  assert.strictEqual(template.buffer, theDataview.buffer);
+}
 
-  // Test for creating dataview with ArrayBuffer and invalid range
-  {
-    const buffer = new ArrayBuffer(128);
-    assert.throws(() => {
-      test_dataview.CreateDataView(buffer, 10, 200);
-    }, RangeError);
-  }
+// Test for creating dataview with ArrayBuffer and invalid range
+{
+  const buffer = new ArrayBuffer(128);
+  assert.throws(() => {
+    test_dataview.CreateDataView(buffer, 10, 200);
+  }, RangeError);
+}
 
-  // Test for creating dataview with SharedArrayBuffer and invalid range
-  {
-    const buffer = new SharedArrayBuffer(128);
-    assert.throws(() => {
-      test_dataview.CreateDataView(buffer, 10, 200);
-    }, RangeError);
-  }
+// Test for creating dataview with SharedArrayBuffer and invalid range
+{
+  const buffer = new SharedArrayBuffer(128);
+  assert.throws(() => {
+    test_dataview.CreateDataView(buffer, 10, 200);
+  }, RangeError);
 }
