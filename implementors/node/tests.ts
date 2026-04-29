9d41ee6b2aa8cb0e0c119798ff2bba4f194d5821
Balakrishna Avulapati <bala.avulapati@holepunch.to>
2026-04-29 14:53:54 +0530

    Update implementors/node/tests.ts

--- implementors/node/tests.ts
+++ implementors/node/tests.ts
@@ -26,7 +26,7 @@ const LOAD_ADDON_MODULE_PATH = path.join(
   ROOT_PATH,
   "implementors",
   "node",
-  "load-addon.js",
+  "load-addon.js"
 );
 const GC_MODULE_PATH = path.join(ROOT_PATH, "implementors", "node", "gc.js");
 const MUST_CALL_MODULE_PATH = path.join(
