204169b21ca9efe999c650a022ab10614683c4f6
Balakrishna Avulapati <ba@bavulapati.com>
2026-06-05 20:17:45 +0530

    feat: port test_dataview to CTS (#39)
    * feat: port test_dataview to CTS
    
    Ports
    [https://github.com/nodejs/node/tree/main/test/js-native-api/test_dataview](test_dataview)
    from Node.js test suite to the CTS.
    This test contains an experimental feature `SharedArrayBuffer`,
    resulting in usage of `add_node_api_cts_experimental_addon()`.
    
    Signed-off-by: Balakrishna Avulapati <ba@bavulapati.com>

---
+++ tests/js-native-api/test_dataview/test.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// Testing api calls for dataview
+const test_dataview = loadAddon("test_dataview");
+
+// Test for creating dataview with ArrayBuffer
+{
+  const buffer = new ArrayBuffer(128);
+  const template = Reflect.construct(DataView, [buffer]);
+
+  const theDataview = test_dataview.CreateDataViewFromJSDataView(template);
+  assert.ok(
+    theDataview instanceof DataView,
+    `Expect ${theDataview} to be a DataView`,
+  );
+}
+
+// Test for creating dataview with ArrayBuffer and invalid range
+{
+  const buffer = new ArrayBuffer(128);
+  assert.throws(() => {
+    test_dataview.CreateDataView(buffer, 10, 200);
+  }, RangeError);
+}
